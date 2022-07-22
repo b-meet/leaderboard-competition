b63772a75dbdfbfabf9a117cfc8d59039c0a574f
Meet Bhalodiya <82386196+b-meet@users.noreply.github.com>
2022-07-22 15:08:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Leaderboard from "./Components/Leaderboard";
+
+function App() {
+	return (
+		<article className='container'>
+			<Leaderboard />
+		</article>
+	);
+}
+
+export default App;
---
+++ src/Styles/styles.js
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export const PlayerContainer = styled.section`
+	padding: 0.5rem 0;
+	max-width: 87%;
+	margin: auto;
+	display: flex;
+	border-bottom: 2px solid rgb(243, 240, 241);
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const PlayerInfo = styled.div`
+	width: 90%;
+	display: flex;
+	align-items: center;
+	color: rgb(58, 53, 55);
+`;
+
+export const Rank = styled.span`
+	width: 30px;
+	height: 30px;
+	color: rgb(255, 255, 255);
+	// background-color: rgb(131, 176, 243);
+	background: ${({ position }) =>
+		position === 0
+			? "rgb(245, 59, 62)"
+			: position === 1
+			? "rgb(253, 123, 65)"
+			: position === 2
+			? "rgb(253, 161, 53)"
+			: "rgb(131, 176, 243)"};
+	display: grid;
+	place-items: center;
+	border-radius: 50%;
+`;
+
+export const Image = styled.img`
+	height: 35px;
+	width: 35px;
+	object-fit: cover;
+	border-radius: 50%;
+	margin: 0 1rem;
+	border: 2px solid #fff;
+`;
+
+export const Points = styled.p`
+	color: rgb(175, 114, 130);
+`;
+
+// @property --num {
+//   syntax: "<integer>";
+//   initial-value: 0;
+//   inherits: false;
+// }
+
+// Points p {
+//   animation: counter 5s infinite alternate ease-in-out;
+//   counter-reset: num var(--num);
+// }
+
+// Points p::after {
+//   content: counter(num);
+// }
+
+// @keyframes counter {
+//   from {
+//     --num: 0;
+//   }
+
+//   to {
+//     --num: 100;
+//   }
+// }
